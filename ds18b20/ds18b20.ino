ee5dba0eb5fb65cb5c072d56054730fb4b10cf9b
hywing <2112093745@qq.com>
2024-06-09 16:20:13 +0800

    [feat] add ds18b20 example

---
+++ ds18b20/ds18b20.ino
@@ -0,0 +1,48 @@
+#include <DS18B20.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// 定义屏幕宽度和高度
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+// 创建一个Adafruit_SSD1306对象。参数为复位引脚，由于许多模块没有使用，因此传递-1。
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+DS18B20 ds(6);
+
+void refresh(float value) 
+{
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0,25);
+  String str1 = " ";
+  String str2 = " `C";
+  String str = str1 + value + str2;
+  display.println(str.c_str());
+  display.display();
+  Serial.println(str);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  // 初始化OLED显示屏
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  
+  display.clearDisplay();
+  display.setTextSize(1);    
+  display.setTextColor(SSD1306_WHITE); 
+  display.display();
+}
+
+void loop() {
+  while (ds.selectNext()) {
+    float value = ds.getTempC();
+    refresh(value);
+  }
+}
